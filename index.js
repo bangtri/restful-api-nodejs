a43eed6d213e61899603d70bd68f7420c16caa3b
bangtri <tribang.soict.hust@gmail.com>
2022-03-28 23:03:41 +0700

    update protect api with keycloak

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 require("dotenv").config();
+var { NodeAdapter } = require("keycloak-nodejs-connect");
+var config = require("./config.json");
+const keycloak = new NodeAdapter(config)
 let bodyParser = require('body-parser');
 let express = require('express');
 let app = express();
+var session = require('express-session');
+var memoryStore = new session.MemoryStore();
 let port = process.env.APPPORT || 8080;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 app.use(bodyParser.json());
+app.use(session({
+    secret: 'ADtAUScX8FCWEXvOOz7doDUBZzyO3KpF',
+    resave: false,
+    saveUninitialized: true,
+    store: memoryStore
+}));
+app.use(keycloak.middleware({ logout: '/logout' }))
 require("./src/router/router")(app);
 app.listen(port);
 app.get('/', function (req, res) {
--- src/router/router.js
+++ src/router/router.js
@@ -1,5 +1,8 @@
 module.exports = function (router) {
     const User = require("../controller/user");
-    router.get("/users", User.findAll);
+    var { NodeAdapter } = require("keycloak-nodejs-connect");
+    var config = require("../../config.json");
+    const keycloak = new NodeAdapter(config)
+    router.get("/users", keycloak.protect('realm:verify-identifications'), User.findAll);
     router.get("/users/:id", User.findById);
 };
