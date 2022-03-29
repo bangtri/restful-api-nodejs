414d026b6b4a20f11c12332f2a15e6cb0d7c41a8
bangtri <tribang.soict.hust@gmail.com>
2022-03-29 22:15:09 +0700

    Update router.js

--- src/router/router.js
+++ src/router/router.js
@@ -3,7 +3,7 @@ module.exports = function (router) {
     var { NodeAdapter } = require("keycloak-nodejs-connect");
     var config = require("../../config.json");
     const keycloak = new NodeAdapter(config)
-    router.get("/users", keycloak.protect('realm:verify-identifications'), User.findAll);
-    router.get("/users/:id", keycloak.protect('realm:verify-identifications'), User.findById);
-    router.post("/users/search", keycloak.protect('realm:verify-identifications'), User.findByName);
+    router.get("/users", keycloak.protect('realm:view-user'), User.findAll);
+    router.get("/users/:id", keycloak.protect('realm:view-user'), User.findById);
+    router.post("/users/search", keycloak.protect('realm:view-user'), User.findByName);
 };
